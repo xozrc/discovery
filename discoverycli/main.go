ed1efbab8288ff22706f253629659dc20e203314
U-WAYNE\wayne <WAYNE+wayne@wayne.netherfire.com>
2016-04-11 18:09:57 +0800

    init

--- discoverycli/main.go
+++
@@ -1,8 +0,0 @@
-package main
-
-import(
-	"github.com/xozrc/discovery/discoverycli/cli"
-)
-func main() {
-	cli.Start()
-}
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/xozrc/discovery/cli"
+)
+
+func main() {
+	cli.Start()
+}
